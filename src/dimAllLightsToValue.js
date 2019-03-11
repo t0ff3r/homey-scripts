f04567ae4c4ab1b1c6cee897a14f6340957bc505
Christoffer Winter-Hjelm <christoffer.winter-hjelm@netlight.com>
2019-03-11 23:00:05 +0100

    refactor(dimAllLightsToValue): Adds a try-catch to catch errors in processLights func

--- src/dimAllLightsToValue.js
+++ src/dimAllLightsToValue.js
@@ -4,28 +4,32 @@ if (!brightness) return;
 
 const devices = await Homey.devices.getDevices();
 const lights = _.filter(devices, device =>
-    device.class === 'light' && _.indexOf(device.capabilities, 'dim') > -1);
+  device.class === 'light' && _.indexOf(device.capabilities, 'dim') > -1);
 
 const dimLightTo = async (light) => {
-    const isLightOn = light.capabilitiesObj.onoff.value;
-    const currentDim = light.capabilitiesObj.dim.value;
+  const isLightOn = light.capabilitiesObj.onoff.value;
+  const currentDim = light.capabilitiesObj.dim.value;
 
-    if (!isLightOn) return;
-    if (currentDim === brightness) return;
+  if (!isLightOn) return;
+  if (currentDim === brightness) return;
 
-    await Homey.devices.setCapabilityValue({
-        deviceId: light.id,
-        capabilityId: 'dim',
-        value: brightness,
-    });
+  await Homey.devices.setCapabilityValue({
+    deviceId: light.id,
+    capabilityId: 'dim',
+    value: brightness,
+  });
 }
 
 async function processLights(lights) {
   for (const light of lights) {
-    await dimLightTo(light);
+    try {
+      await dimLightTo(light);
+    } catch (err) {
+      // Log this please
+    }
   }
 }
 
 await processLights(lights);
 
-return lights;
+return 'success';
